84b61f95bbb274a5b3e510507544ee5a02d6d120
jéssica senger <jessicasbogoni1@gmail.com>
2021-11-06 18:20:05 -0400

    refactor(Home): cartItemsAmount

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,9 +24,10 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  const cartItemsAmount = cart.reduce((sumAmount, product) => {
-    return { ...sumAmount, [product.id]: product.amount };
-  }, {} as CartItemsAmount);
+  const cartItemsAmount = cart.reduce(
+    (sumAmount, product) => ({ ...sumAmount, [product.id]: product.amount }),
+    {} as CartItemsAmount
+  );
 
   useEffect(() => {
     async function loadProducts() {
